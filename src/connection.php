4d59120396e25a888c5148415b76fb15f27ef44c
Charles Seymour <charlesseymour@yahoo.com>
2019-08-23 13:04:09 -0500

    Added database connection file

---
+++ src/connection.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 'On');
+try {
+	$db = new PDO('sqlite:./blog.db');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(Exception $e) {
+	echo $e->getMessage();
+	die();
+}
+?>
--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php 
 
+require_once('connection.php');
 // Routes
 
 $app->get('/', function ($request, $response, $args) {
