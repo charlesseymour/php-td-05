f876b025dda8bbacef94b3cb97db951dd0118dd3
Charles Seymour <charlesseymour@yahoo.com>
2019-08-26 18:14:30 -0500

    Add new post

--- models/Post.php
+++ models/Post.php
@@ -1,10 +1,12 @@
 <?php 
-
+//namespace Models;
 use Illuminate\Database\Eloquent\Model;
 include('Comment.php');
 
 class Post extends Model {
 
+	public $timestamps = false;
+	
 	public function comments() 
 	{
 		return $this->hasMany('Comment');
--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,7 @@
 <?php 
 
 include("../models/Post.php");
-
-// require_once('connection.php');
+//use \Models\Post;
 // Routes
 
 $app->get('/blog/{id}', function ($request, $response, $args) {
@@ -11,7 +10,17 @@ $app->get('/blog/{id}', function ($request, $response, $args) {
 	return $this->view->render($response, 'detail.phtml', ["post" => $post, "comments" => $comments]);
 });
 
+$app->map(['GET', 'POST'], '/new', function ($request, $response, $args) {
+	$nameKey = $this->csrf->getTokenNameKey();
+    $valueKey = $this->csrf->getTokenValueKey();
+    $name = $request->getAttribute($nameKey);
+    $value = $request->getAttribute($valueKey);
+	return $this->view->render($response, 'new.phtml', [
+		"nameKey" => $nameKey, "valueKey" => $valueKey, "name" => $name, "value" => $value
+	]);
+});
+
 $app->get('/', function ($request, $response, $args) {
-	$posts = Post::take(3)->get();
+	$posts = Post::orderBy('date', 'desc')->take(3)->get();
 	return $this->view->render($response, 'posts.phtml', ["posts" => $posts]);
 });
