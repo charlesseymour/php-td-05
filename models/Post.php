a044db86e8329dddf103fe696f508ff230d0f328
Charles Seymour <charlesseymour@yahoo.com>
2019-08-26 13:27:35 -0500

    Detail view with comments

--- models/Post.php
+++ models/Post.php
@@ -1,9 +1,14 @@
 <?php 
 
 use Illuminate\Database\Eloquent\Model;
+include('Comment.php');
 
 class Post extends Model {
 
+	public function comments() 
+	{
+		return $this->hasMany('Comment');
+	}
 }
 
 ?>
--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,12 @@ include("../models/Post.php");
 // require_once('connection.php');
 // Routes
 
+$app->get('/blog/{id}', function ($request, $response, $args) {
+	$post = Post::where('id', $args['id'])->first();
+	$comments = $post->comments;
+	return $this->view->render($response, 'detail.phtml', ["post" => $post, "comments" => $comments]);
+});
+
 $app->get('/', function ($request, $response, $args) {
 	$posts = Post::take(3)->get();
 	return $this->view->render($response, 'posts.phtml', ["posts" => $posts]);
